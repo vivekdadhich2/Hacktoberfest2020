ca28056d9158cfeb1a126bf49ce26393d2ca7802
vivekdadhich2 <72310623+vivekdadhich2@users.noreply.github.com>
2020-10-03 23:46:44 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,17 @@
+package Java;
+
+public class Sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a =  1;
+		int b = 2;
+		var c = a + b;
+		System.out.println(c);
+		System.out.print("dadhichvivek1\"Hacktoberfest2020");
+				
+	}
+
+}
+
